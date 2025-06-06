dbfc5677e434389001632056d049593622ccd0a5
Rajendrakumar p <rajendrakumarp99@gmail.com>
2025-06-06 13:58:44 +0530

    first commit

---
+++ src/main/java/com/rajendra/RajendraProductApplication.java
@@ -0,0 +1,13 @@
+package com.rajendra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RajendraProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RajendraProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rajendra/RajendraProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajendra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RajendraProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
